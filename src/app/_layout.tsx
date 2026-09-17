a87081f38c4676d1d468e4ee179bab5b202ce258
dragan <vucicdragan23@gmail.com>
2026-09-17 21:19:06 +0200

    Start page created

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,66 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import { Stack } from "expo-router";
+import { Image, StyleSheet, View, Pressable } from "react-native";
+import Feather from '@expo/vector-icons/Feather';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
+import Octicons from '@expo/vector-icons/Octicons';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function RootLayout() {
+  const insets = useSafeAreaInsets();
+  const bottomPadding = Math.max(insets.bottom - 10, 0);
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+  <View style={styles.container}>
+   <View style={[styles.header, { paddingTop: insets.top}]}>
+          <Image style={styles.tinyLogo} source={require("../../assets/images/villa_bomada_white.png")}/>
+          <Pressable onPress={() => console.log("Settings pressed")}><Feather name="settings" size={30} color="white" /></Pressable>
+    </View>
+
+  <View style={styles.container}>
+    <Stack screenOptions={{ headerShown: false}} />
+  </View>
+
+  <View style={[styles.footer, { paddingBottom: bottomPadding }]}>
+        <Pressable onPress={() => console.log("Calendar pressed")}><EvilIcons name="calendar" size={50} color="white" /></Pressable>
+        <Pressable onPress={() => console.log("Checklist pressed")}><Octicons name="checklist" size={30} color="white" /></Pressable>
+        <Pressable onPress={() => console.log("Add button pressed")}><AntDesign name="plus-circle" size={40} color="white" style={styles.addButton} /></Pressable>
+        <Pressable onPress={() => console.log("Guests pressed")}><Ionicons name="people-outline" size={38} color="white" /></Pressable>
+        <Pressable onPress={() => console.log("Calendar Search pressed")}><MaterialCommunityIcons name="calendar-search" size={30} color="white" /></Pressable>
+      </View>
+    
+      </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: "#5D70E5",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+  },
+  tinyLogo: {
+    resizeMode: "contain",
+    width: 100,
+    height: 50,
+  },
+  footer: {
+    backgroundColor: "#5D70E5",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    paddingVertical: 20,
+  },
+  addButton: {
+    marginTop: -20,
+  },
+});
