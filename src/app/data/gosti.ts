f132f9e32566861c701face626c76ce017f444f7
dragan <vucicdragan23@gmail.com>
2026-09-17 22:57:35 +0200

    Added basic guest data

---
+++ src/app/data/gosti.ts
@@ -0,0 +1,38 @@
+export const bojaZaTipGosta = {
+    booking: "#BCFFCB",
+    direkt: "#B1D8FD",
+    prijatelji: "#FFF9BC"
+}
+
+export const gosti = [
+    {
+        id: 1,
+        ime: "Marko Marić",
+        apartman: "A1",
+        odDatum: "2026-07-14",
+        doDatum: "2026-07-21",
+        tipGosta: "booking",
+        odrasli: 2,
+        djeca: 0
+    }, 
+    {
+        id: 2,
+        ime: "Ana Anić",
+        apartman: "A2",
+        odDatum: "2026-07-15",
+        doDatum: "2026-07-22",
+        tipGosta: "direkt",
+        odrasli: 2,
+        djeca: 2
+    },
+    {
+        id: 3,
+        ime: "Ivana Ivić",
+        apartman: "A5",
+        odDatum: "2026-07-16",
+        doDatum: "2026-07-23",
+        tipGosta: "prijatelji",
+        odrasli: 2,
+        djeca: 1
+    }
+]
